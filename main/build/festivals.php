ddae61e7e2abfeba429c9e477b2d97277523f79d
Berke Ates <wppgamer@users.noreply.github.com>
2017-07-05 18:12:44 +0200

    tidy up + festival page

---
+++ main/build/festivals.php
@@ -0,0 +1,102 @@
+
+<?php
+	include "header.php";
+ ?>
+	<body>
+
+	<div id="fh5co-page">
+		<?php
+			include "topbar.php";
+		 ?>
+
+
+		<div class="fh5co-cover fh5co-cover-style-2 js-full-height" data-stellar-background-ratio="0.5" data-next="yes"  style="background-image: url(../images/png.jpg);">
+		  	<span class="scroll-btn wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.4s">
+				<a href="#">
+					<span class="mouse"><span></span></span>
+				</a>
+			</span>
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover-text">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-push-6 col-md-6 full-height js-full-height">
+							<div class="fh5co-cover-intro">
+								<h1 class="cover-text-lead wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">Festivals</h1>
+								<h2 class="cover-text-sublead wow fadeInUp" data-wow-duration="1s" data-wow-delay=".8s">Hier findest du eine Liste der aktuellen Festivals / Wettbewerbe</h2>
+								<p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.1s"><a class="btn btn-primary btn-outline btn-lg">Festival eintragen</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+		<div class="fh5co-project-style-2">
+			<div class="container">
+				<div class="row p-b">
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<h2 class="fh5co-heading wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">Festivals</h2>
+						<p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".8s">Hier findest du eine Liste der aktuellen Festivals / Wettbewerbe</p>
+						<p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.1s"><a href="#" class="btn btn-success btn-lg">Festival eintragen</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="fh5co-projects">
+				<div class="container" style="width:100%;">
+        <div class="row">
+				<ul>
+					<li class="wow fadeInUp col-md-4" style="background-image: url(../images/full_4.jpg);" data-wow-duration="1s" data-wow-delay="1.4s" data-stellar-background-ratio="0.5">
+						<a href="#">
+							<div class="fh5co-overlay"></div>
+								<div class="fh5co-text">
+									<div class="fh5co-text-inner">
+											<div class="text-center"><h3>Project Name #1</h3></div>
+									</div>
+								</div>
+						</a>
+					</li>
+
+					<li class="wow fadeInUp col-md-4" style="background-image: url(../images/full_2.jpg);" data-wow-duration="1s" data-wow-delay="1.7s" data-stellar-background-ratio="0.5">
+						<a href="#">
+							<div class="fh5co-overlay"></div>
+								<div class="fh5co-text">
+									<div class="fh5co-text-inner">
+											<div class="text-center"><h3>Project Name #2</h3></div>
+									</div>
+								</div>
+						</a>
+					</li>
+
+					<li class="wow fadeInUp col-md-4" style="background-image: url(../images/full_1.jpg);" data-wow-duration="1s" data-wow-delay="2s" data-stellar-background-ratio="0.5">
+						<a href="#">
+							<div class="fh5co-overlay"></div>
+								<div class="fh5co-text">
+									<div class="fh5co-text-inner">
+											<div class="text-center"><h3>Project Name #3</h3></div>
+								</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+  </div>
+</div>
+
+		<?php
+		include "footer.php";
+		?>
+		<!-- END footer -->
+
+	</div>
+	<!-- END page-->
+	<?php
+	include "js.php";
+	 ?>
+	</body>
+</html>
---
+++ main/build/topbar.php
@@ -0,0 +1,23 @@
+<nav class="fh5co-nav-style-1" role="navigation" data-offcanvass-position="fh5co-offcanvass-left">
+  <div class="container">
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 fh5co-logo">
+      <a href="#" class="js-fh5co-mobile-toggle fh5co-nav-toggle"><i></i></a>
+      <a href="#">Jugend-Theater</a>
+    </div>
+    <div class="col-lg-6 col-md-5 col-sm-5 text-center fh5co-link-wrap">
+      <ul data-offcanvass="yes">
+        <li><a href="../index.php">Home</a></li>
+      <!--  <li class="active"><a href="build/festivals.php">Festivals</a></li> -->
+      <li><a href="build/festivals.php">Festivals</a></li>
+        <li><a href="#">Info</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-4 text-right fh5co-link-wrap">
+      <ul class="fh5co-special" data-offcanvass="yes">
+        <li><a href="#">Login</a></li>
+        <li><a href="#" class="call-to-action">Register</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
