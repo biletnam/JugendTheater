81d42515e4867c758f20a6cb74962ecb52432e3d
Berke Ates <wppgamer@users.noreply.github.com>
2017-07-24 15:18:24 +0200

    removed node and moved admin

---
+++ main/admin/sites/login.php
@@ -0,0 +1,26 @@
+<div class="fh5co-cover fh5co-cover-style-2 js-full-height" data-stellar-background-ratio="0.5" data-next="yes"  style="background-image: url(images/edit/home.jpg);">
+  <div class="fh5co-overlay"></div>
+  <div class="fh5co-cover-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-push-6 col-md-6 full-height js-full-height">
+          <div class="fh5co-cover-intro">
+            <h1 class="cover-text-lead wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">Admin-Panel</h1>
+            <form action="functions/functions.php?func=login" method="post">
+              <div class="col-md-6 resPw wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
+                  <input class="form-control input-lg mt-1" maxlength="50" name="Us" id="adminUser" type="text" placeholder="Username" required>
+              </div>
+              <div class="col-md-6 resPw wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
+                  <input class="form-control input-lg mt-1" maxlength="100" name="Pw" id="adminPw" type="password" placeholder="Passwort" required>
+              </div>
+              <div class="col-md-6 resPw"></div>
+              <div class="col-md-6 resPw wow fadeInUp" data-wow-duration="1s" data-wow-delay=".8s">
+                <input id="resetPwBtn" type="submit" class="btn btn-invert btn-outline pull-right" value="Login">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
